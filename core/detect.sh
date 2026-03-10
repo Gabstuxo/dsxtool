66bf83e6708aedd57b06352c0cbcb07492a389cb
Carlos Gabriel <oditorrinco222@gmail.com>
2026-03-10 11:25:08 -0300

    fix

--- core/detect.sh
+++ core/detect.sh
@@ -1,49 +1,48 @@
 detect_distro() {
-    if [[ ! -f /etc/os-release ]]; then
+
+    [[ -f /etc/os-release ]] || {
         echo "Cannot detect distribution: /etc/os-release not found"
         exit 1
-    fi
+    }
 
-    
     source /etc/os-release
 
-    DISTRO_RAW="${ID,,}"
-    DISTRO_LIKE="${ID_LIKE,,}"
+    DISTRO_RAW="${ID:-}"
+    DISTRO_RAW="${DISTRO_RAW,,}"
+
+    DISTRO_LIKE="${ID_LIKE:-}"
+    DISTRO_LIKE="${DISTRO_LIKE,,}"
 
     case "$DISTRO_RAW" in
-    
+
         arch|manjaro|endeavouros|garuda|artix)
             DISTRO="arch"
             ;;
 
-       
         debian|ubuntu|pop|linuxmint|zorin|elementary|kali|neon)
             DISTRO="debian"
             ;;
 
-    
         fedora|rhel|centos|rocky|almalinux|ol)
             DISTRO="fedora"
             ;;
 
-    
         opensuse*|sles|sled)
             DISTRO="suse"
             ;;
-
 
         void)
             DISTRO="void"
             ;;
 
         *)
-            if [[ "$DISTRO_LIKE" == *"arch"* ]]; then
+            if [[ "$DISTRO_LIKE" == *arch* ]]; then
                 DISTRO="arch"
-            elif [[ "$DISTRO_LIKE" == *"debian"* ]] || [[ "$DISTRO_LIKE" == *"ubuntu"* ]]; then
+            elif [[ "$DISTRO_LIKE" == *debian* ]] || [[ "$DISTRO_LIKE" == *ubuntu* ]]; then
                 DISTRO="debian"
-            elif [[ "$DISTRO_LIKE" == *"rhel"* ]] || [[ "$DISTRO_LIKE" == *"fedora"* ]]; then
+            elif [[ "$DISTRO_LIKE" == *rhel* ]] || [[ "$DISTRO_LIKE" == *fedora* ]]; then
                 DISTRO="fedora"
-            elif [[ "$DISTRO_LIKE" == *"suse"* ]]; then
+            elif [[ "$DISTRO_LIKE" == *suse* ]]; then
                 DISTRO="suse"
             else
                 echo "Unsupported distribution: $DISTRO_RAW"
